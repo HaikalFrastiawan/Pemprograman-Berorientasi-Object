da8bf61cc9aceb66a5dccce1fff3973312cda26a
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2026-03-31 20:18:06 +0700

    Add input handling for user details in Java

---
+++ minggu2_HaikalFrastiawan/input.java
@@ -0,0 +1,34 @@
+
+    package minggu2.haikalfrastiawan;
+
+    import java.util.Scanner;
+    public class Minggu2HaikalFrastiawan {
+
+        public static void main(String[] args) {
+        Scanner masuk = new Scanner(System.in);
+
+        String nama;
+        char GolonganDarah;
+        int umur;
+
+        System.out.print("Masukan Nama Anda : ");
+        nama = masuk.nextLine();
+        
+        
+        System.out.print("Masukan Golongan Darah Anda : ");
+        GolonganDarah = masuk.next().charAt(0);
+        
+        
+        System.out.print("Masukan Umur Anda : ");
+        umur = masuk.nextInt();
+        
+        
+        System.out.println("\nNama Anda  : " + nama);
+        System.out.println("Nilai Anda : " + GolonganDarah );
+        System.out.println("Nilai Anda : " + umur );
+        
+        
+        masuk.close();
+        }
+
+    }
