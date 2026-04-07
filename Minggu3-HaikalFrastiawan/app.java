4db3ce36ac761cc52f02aa73369ebf66e6ec73be
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2026-04-07 19:19:29 +0700

    Update app.java

--- Minggu3-HaikalFrastiawan/app.java
+++ Minggu3-HaikalFrastiawan/app.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
 
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+
+        int bilangan;
+        System.out.print("Masukan Sebuah Bilangan: ");
+        bilangan = in.nextInt();
+
+        if (bilangan % 2 == 0) {
+            System.out.println(bilangan + " Adalah Bilangan Genap");
+        } else {
+            System.out.println(bilangan + " Adalah Bilangan Ganjil");
+        }
+    }
+}
