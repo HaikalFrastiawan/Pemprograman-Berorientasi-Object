05260763122d527dfe882937abea33f223f876e2
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2026-05-11 21:55:35 +0700

    Refactor Latihan2 class and main method structure

--- Minggu7-HaikalFrastiawan/Latihan2.java
+++ Minggu7-HaikalFrastiawan/Latihan2.java
@@ -1,79 +1,36 @@
 import java.util.Scanner;
 
-public class Latihan2 {
+class Latihan2 {
+
+    double pembelian;
+
+
+    public double potong() {
+        if (pembelian < 1000000) {
+            return 0;
+        } else if (pembelian >= 1000000 && pembelian <= 5000000) {
+            return 0.20 * pembelian;
+        } else {
+            return 0.35 * pembelian; 
+        }
+    }
+
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
+        Latihan2 obj = new Latihan2();
 
-       
-        int banyakJenis;
-        String[] jenisPotong = new String[10];
-        char[] kodePotong = new char[10];
-        int[] hargaSatuan = new int[10];
-        int[] banyakBeli = new int[10];
-        int[] jumlahHarga = new int[10];
+
+        System.out.print("Besar pembelian barang Rp. ");
+        obj.pembelian = scan.nextDouble();
+
+
+        double besarDiskon = obj.potong();
+        double totalBayar = obj.pembelian - besarDiskon;
+
+
+        System.out.println("Besar diskon yang diberikan Rp. " + (long)besarDiskon);
+        System.out.println("Besar harga yang harus dibayarkan Rp. " + (long)totalBayar);
         
-        int jumlahBayar = 0;
-        double pajak, totalBayar;
-
-        
-        System.out.println("GEROBAK FRIED CHICKEN");
-        System.out.println("---------------------------");
-        System.out.println("Kode   Jenis      Harga");
-        System.out.println("---------------------------");
-        System.out.println("D      Dada       Rp. 2500");
-        System.out.println("P      Paha       Rp. 2000");
-        System.out.println("S      Sayap      Rp. 1500");
-        System.out.println("---------------------------");
-
-        System.out.print("Banyak Jenis : ");
-        banyakJenis = input.nextInt();
-
-        
-        for (int i = 0; i < banyakJenis; i++) {
-            System.out.println("\nJenis Ke - " + (i + 1));
-            System.out.print("Jenis Potong [D/P/S] : ");
-            kodePotong[i] = input.next().toUpperCase().charAt(0);
-            System.out.print("Banyak Potong        : ");
-            banyakBeli[i] = input.nextInt();
-
-            
-            if (kodePotong[i] == 'D') {
-                jenisPotong[i] = "Dada ";
-                hargaSatuan[i] = 2500;
-            } else if (kodePotong[i] == 'P') {
-                jenisPotong[i] = "Paha ";
-                hargaSatuan[i] = 2000;
-            } else if (kodePotong[i] == 'S') {
-                jenisPotong[i] = "Sayap";
-                hargaSatuan[i] = 1500;
-            } else {
-                jenisPotong[i] = "-----";
-                hargaSatuan[i] = 0;
-            }
-
-            jumlahHarga[i] = hargaSatuan[i] * banyakBeli[i];
-            jumlahBayar += jumlahHarga[i];
-        }
-
-        
-        pajak = jumlahBayar * 0.10;
-        totalBayar = jumlahBayar + pajak;
-
-        
-        System.out.println("\n\nGEROBAK FRIED CHICKEN");
-        System.out.println("---------------------------------------------------------");
-        System.out.println("No.  Jenis        Harga        Banyak       Jumlah");
-        System.out.println("     Potong       Satuan       Beli         Harga");
-        System.out.println("---------------------------------------------------------");
-        
-        for (int i = 0; i < banyakJenis; i++) {
-            System.out.printf("%d    %s        Rp. %-8d %-10d Rp. %d\n", 
-                (i + 1), jenisPotong[i], hargaSatuan[i], banyakBeli[i], jumlahHarga[i]);
-        }
-
-        System.out.println("---------------------------------------------------------");
-        System.out.printf("                        Jumlah Bayar  Rp. %d\n", jumlahBayar);
-        System.out.printf("                        Pajak 10%%     Rp. %.0f\n", pajak);
-        System.out.printf("                        Total Bayar   Rp. %.0f\n", totalBayar);
+        scan.close();
     }
 }
