800346ea8474695833109967f19443ac6801da2f
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2026-05-12 07:07:42 +0700

    Refactor program for employee honor to student grades

--- minggu6-HaikalFrastiawan/Latihan3.java
+++ minggu6-HaikalFrastiawan/Latihan3.java
@@ -4,77 +4,55 @@ public class Latihan3 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Program Hitung Honor Karyawan Kontrak PT. EASY");
-        System.out.print("Masukkan Jumlah Karyawan : ");
+        System.out.println("PROGRAM HITUNG NILAI AKHIR MATERI PEMROGRAMMAN C++");
+        System.out.print("Masukkan Jumlah Mahasiswa : ");
         int jumlah = input.nextInt();
 
-        // Siapkan tempat penyimpanan data menggunakan array dasar
+        // Deklarasi array untuk menyimpan data
         String[] nama = new String[jumlah];
-        int[] gol = new int[jumlah];
-        int[] pend = new int[jumlah];
-        int[] jam = new int[jumlah];
-        
-        double[] tunjJab = new double[jumlah];
-        double[] tunjPen = new double[jumlah];
-        double[] lembur = new double[jumlah];
-        double[] gajiBersih = new double[jumlah];
+        double[] nTugas = new double[jumlah];
+        double[] nUTS = new double[jumlah];
+        double[] nUAS = new double[jumlah];
+        double[] nAkhir = new double[jumlah];
+        char[] grade = new char[jumlah];
 
-        int honorTetap = 700000;
-
-        // Proses Input dan Perhitungan Langsung
+        // Proses Input Data
         for (int i = 0; i < jumlah; i++) {
-            System.out.println("\nKaryawan Ke - " + (i + 1));
-            System.out.print("Nama Karyawan      : ");
-            input.nextLine(); // Pembersih enter
+            System.out.println("\nMahasiswa Ke - " + (i + 1));
+            input.nextLine(); // Membersihkan buffer
+            System.out.print("Nama Mahasiswa : ");
             nama[i] = input.nextLine();
-            System.out.print("Golongan (1/2/3)   : ");
-            gol[i] = input.nextInt();
-            System.out.print("Pendidikan (1=SMU/2=D3/3=S1) : ");
-            pend[i] = input.nextInt();
-            System.out.print("Jumlah Jam Kerja   : ");
-            jam[i] = input.nextInt();
+            System.out.print("Nilai Tugas    : ");
+            nTugas[i] = input.nextDouble();
+            System.out.print("Nilai UTS      : ");
+            nUTS[i] = input.nextDouble();
+            System.out.print("Nilai UAS      : ");
+            nUAS[i] = input.nextDouble();
 
-            // Hitung Tunjangan Jabatan
-            if (gol[i] == 1) tunjJab[i] = 0.05 * honorTetap;
-            else if (gol[i] == 2) tunjJab[i] = 0.10 * honorTetap;
-            else if (gol[i] == 3) tunjJab[i] = 0.15 * honorTetap;
+            // Hitung Nilai Akhir
+            nAkhir[i] = (nTugas[i] * 0.3) + (nUTS[i] * 0.3) + (nUAS[i] * 0.4);
 
-            // Hitung Tunjangan Pendidikan
-            if (pend[i] == 1) tunjPen[i] = 0.025 * honorTetap;
-            else if (pend[i] == 2) tunjPen[i] = 0.05 * honorTetap;
-            else if (pend[i] == 3) tunjPen[i] = 0.075 * honorTetap;
-
-            // Hitung Lembur
-            if (jam[i] > 240) {
-                lembur[i] = (jam[i] - 240) * 2500;
-            } else {
-                lembur[i] = 0;
-            }
-
-            // Hitung Total
-            gajiBersih[i] = honorTetap + tunjJab[i] + tunjPen[i] + lembur[i];
+            // Penentuan Grade
+            if (nAkhir[i] >= 80) grade[i] = 'A';
+            else if (nAkhir[i] >= 70) grade[i] = 'B';
+            else if (nAkhir[i] >= 59) grade[i] = 'C';
+            else if (nAkhir[i] >= 50) grade[i] = 'D';
+            else grade[i] = 'E';
         }
 
-        // Tampilan Keluaran
-        System.out.println("\nPT. EASY");
-        System.out.println("---------------------------------------------------------------------------");
-        System.out.println("No. Nama        Tunjangan");
-        System.out.println("    Karyawan    -----------------   Honor    Pajak    Gaji");
-        System.out.println("                Jabatan  Pdidikan   Lembur            Bersih");
-        System.out.println("---------------------------------------------------------------------------");
+        // Tampilan Layar Keluaran
+        System.out.println("\nDAFTAR NILAI");
+        System.out.println("MATERI : PEMROGRAMMAN C++");
+        System.out.println("------------------------------------------------------------------");
+        System.out.println("No.\tNama\t\tNilai\t\t\t\tGrade");
+        System.out.println("Mahasiswa\t------------------------------------------");
+        System.out.println("\t\tTugas\tUTS\tUAS\tAkhir");
+        System.out.println("------------------------------------------------------------------");
 
-        double totalGajiDikeluarkan = 0;
         for (int i = 0; i < jumlah; i++) {
-            System.out.print((i + 1) + "   " + nama[i] + "\t");
-            System.out.print(tunjJab[i] + "\t " + tunjPen[i] + "\t    ");
-            System.out.print(lembur[i] + "\t 0\t  Rp. " + gajiBersih[i] + "\n");
-            
-            totalGajiDikeluarkan += gajiBersih[i];
+            System.out.printf("%d\t%s\t%.0f\t%.0f\t%.0f\t%.1f\t\t%c\n", 
+                (i + 1), nama[i], nTugas[i], nUTS[i], nUAS[i], nAkhir[i], grade[i]);
         }
-
-        System.out.println("---------------------------------------------------------------------------");
-        System.out.println("Total Gaji yang dikeluarkan Rp. " + totalGajiDikeluarkan);
-        
-        input.close();
+        System.out.println("------------------------------------------------------------------");
     }
 }
